0133f5798b83dfb189cd5b5f54f093df10be081c
Iain Baird <iain.baird@gmail.com>
2019-11-27 11:49:04 +0000

    another one

--- public/index.php
+++ public/index.php
@@ -129,13 +129,12 @@ $mobileDevice = mobile_device();
 			</div>
 
             <div class="article">
-                <h2>Push on Feedback</h2>
+                <h2>Another Push on Feedback</h2>
                 <p>We're so keen on feedback that we now have 2 feedback forms.</p>
 
-                <script type="text/javascript">
-                    !function(e,t,r,n,a){if(!e[a]){for(var i=e[a]=[],s=0;s<r.length;s++){var c=r[s];i[c]=i[c]||function(e){return function(){var t=Array.prototype.slice.call(arguments);i.push([e,t])}}(c)}i.SNIPPET_VERSION="1.0.1";var o=t.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/"+n+"/"+a+".js";var l=t.getElementsByTagName("script")[0];l.parentNode.insertBefore(o,l)}}(window,document,["survey","reset","config","init","set","get","event","identify","track","page","screen","group","alias"],"dskVByY3GMMWzlTa","delighted");
-                    delighted.survey();
-                </script>
+                <!-- Appzi: Capture Insightful Feedback -->
+                <script async src="https://app.appzi.io/bootstrap/bundle.js?token=IU9Nz"></script>
+                <!-- End Appzi -->
             </div>
 
             <div class="article">
